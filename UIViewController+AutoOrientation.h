b1eed3c07ada98ce0362c3893539a2dd14f40fc8
Diego Chohfi <dchohfi@gmail.com>
2012-11-12 14:23:30 -0200

    Adicionando tela para landsace e portrait dos detalhes do onibus

---
+++ UIViewController+AutoOrientation.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+AutoOrientation.h
+//  BusaoSP
+//
+//  Created by Diego Chohfi on 11/12/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (AutoOrientation)
+
+@end
---
+++ UIViewController+AutoOrientation.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+AutoOrientation.m
+//  BusaoSP
+//
+//  Created by Diego Chohfi on 11/12/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import "UIViewController+AutoOrientation.h"
+
+@implementation UIViewController (AutoOrientation)
+
+@end
